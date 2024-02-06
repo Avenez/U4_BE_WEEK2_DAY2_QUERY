a0ad230aaff92af09bbe0db2ffb6d07a31c0a96c
Avenez <145536778+Avenez@users.noreply.github.com>
2024-02-06 15:12:04 +0100

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,12 @@
+--select * from Products;
+--select * from Products where UnitsInStock >= 40;
+--select * from Employees where city = 'london';
+
+--select * from Orders order by Freight desc;
+--select * from Orders where Freight >= 90 and Freight <=200 order by Freight desc;
+
+--select * from Products where CategoryID = 1;
+
+--select * from [Order Details] where Discount > 0 order by Discount desc;
+
+--select * from Orders where CustomerID = 'BOTTM' and Freight > 50 order by Freight desc;
